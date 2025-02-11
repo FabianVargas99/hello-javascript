ad32afe06145ca0de96ec20b5c1203559600b3e7
matDev <43002169+FabianVargas99@users.noreply.github.com>
2025-02-11 18:00:05 -0500

    Update 13-structures-exercises.js

--- Basic/13-structures-exercises.js
+++ Basic/13-structures-exercises.js
@@ -14,14 +14,31 @@ myArray.unshift("libelula")
 myArray.splice(2,1)
 
 // 4. Crea un set que almacene cinco libros
+let mySet = new Set(['libro 1','libro 2','libro 3','libro 4','libro 5'])
 
 // 5. Añade dos más. Uno de ellos repetido
+mySet.add('libro 6')
+mySet.add('libro 1')
 
 // 6. Elimina uno concreto a tu elección
-
+mySet.delete('libro 3')
 // 7. Crea un mapa que asocie el número del mes a su nombre
-
+let myMap = new Map([
+  [1,'Enero'],
+  [2,'Febrero'],
+  [3,'Marzo'],
+  [4,'Abril'],
+  [5,'Mayo'],
+  [6,'Junio'],
+  [7,'Julio'],
+  [8,'Agosto'],
+  [9,'Septiembre'],
+  [10,'Octubre'],
+  [11,'Noviembre'],
+  [12,'Diciembre']
+])
 // 8. Comprueba si el mes número 5 existe en el map e imprime su valor
+myMap.has(5) ? console.log(myMap.get(5)) : ''
 
 // 9. Añade al mapa una clave con un array que almacene los meses de verano
 
